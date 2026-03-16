b697f7ef41a3a75b469a4d60d4efbc4e761f3113
RamaKrishna <ramakrishna@inflexiontechfze.com>
2026-03-16 22:23:43 +0530

    initial commit

---
+++ ui-5/webapp/Component.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+], function (UIComponent) {
+    "use strict";
+
+    return UIComponent.extend("demo.app.Component", {
+
+        metadata: {
+            manifest: "json"
+        },
+
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+        }
+
+    });
+});
